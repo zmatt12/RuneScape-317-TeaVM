fdf9d9fc98757ebd139f896051e7fde20ed9c258
mdbell <contact@mdbell.me>
2021-10-31 19:15:46 -0400

    cleaner rectangles pog

--- src/main/java/web/impl/js/webgl/JSGraphicsGL.java
+++ src/main/java/web/impl/js/webgl/JSGraphicsGL.java
@@ -2,7 +2,6 @@ package web.impl.js.webgl;
 
 import org.teavm.jso.dom.html.HTMLCanvasElement;
 import org.teavm.jso.typedarrays.Float32Array;
-import org.teavm.jso.typedarrays.Uint16Array;
 import org.teavm.jso.webgl.*;
 import web.IFont;
 import web.IGraphics;
@@ -16,12 +15,14 @@ public class JSGraphicsGL implements IGraphics {
     private final HTMLCanvasElement canvas;
     private final WebGLRenderingContext gl;
     private final Float32Array arr = Float32Array.create(12);
-    private final Uint16Array indices = Uint16Array.create(4);
+    int aVertexPosition;
     private float[] color = Color.black.asFloatArray();
-    private WebGLProgram fill_rect;
+    //rect program
+    private WebGLProgram draw_rect;
+    private WebGLBuffer rect_buffer;
+    private WebGLUniformLocation rectColorLocation;
+    //drawImage program
     private WebGLProgram draw_img;
-    private WebGLBuffer rect_buffer, indBuf;
-
     private WebGLBuffer imagePositionBuffer, imageCoordBuffer;
     private int positionLocation, texcoordLocation;
     private WebGLUniformLocation imageTexLocation, imageMatrixLocation;
@@ -33,11 +34,12 @@ public class JSGraphicsGL implements IGraphics {
     }
 
     private void init() {
-        fill_rect = WebGL.loadAndCompile(gl, "rect_fill");
+        draw_rect = WebGL.loadAndCompile(gl, "draw_rect");
         draw_img = WebGL.loadAndCompile(gl, "draw_img");
 
         rect_buffer = gl.createBuffer();
-        indBuf = gl.createBuffer();
+        rectColorLocation = gl.getUniformLocation(draw_rect, "uColor");
+        aVertexPosition = gl.getAttribLocation(draw_rect, "aVertexPosition");
 
         imagePositionBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, imagePositionBuffer);
@@ -53,18 +55,15 @@ public class JSGraphicsGL implements IGraphics {
         gl.bufferData(gl.ARRAY_BUFFER, texturePosition, gl.STATIC_DRAW);
         imageCoordBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, imageCoordBuffer);
-
-        // Put texcoords in the buffer
-        float[] texcoords = new float[]{
+        Float32Array texCoords = Float32Array.create(12);
+        texCoords.set(new float[]{
                 0, 0,
                 0, 1,
                 1, 0,
                 1, 0,
                 0, 1,
                 1, 1
-        };
-        Float32Array texCoords = Float32Array.create(12);
-        texCoords.set(texcoords);
+        });
         gl.bufferData(gl.ARRAY_BUFFER, texCoords, gl.STATIC_DRAW);
 
         // look up where the vertex data needs to go.
@@ -83,70 +82,12 @@ public class JSGraphicsGL implements IGraphics {
 
     @Override
     public void fillRect(int x, int y, int width, int height) {
-        x -= canvas.getWidth() / 2;
-        y -= canvas.getHeight() / 2;
-
-        float canvasWidth = canvas.getWidth() / 2f;
-        float canvasHeight = canvas.getHeight() / 2f;
-
-        float startX = x / canvasWidth;
-        float startY = (y / canvasHeight);
-        float endX = (x + width) / canvasWidth;
-        float endY = ((y + height) / canvasHeight);
-
-        arr.set(new float[]{
-                startX, startY,
-                startX, endY,
-                endX, startY, // Triangle 1
-                endX, startY,
-                startX, endY,
-                endX, endY // Triangle 2
-        });
-
-        WebGLBuffer vbuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vbuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, arr, gl.STATIC_DRAW);
-
-        gl.useProgram(fill_rect);
-        WebGLUniformLocation uColor = gl.getUniformLocation(fill_rect, "uColor");
-        gl.uniform4fv(uColor, color);
-
-        int aVertexPosition = gl.getAttribLocation(fill_rect, "aVertexPosition");
-        gl.enableVertexAttribArray(aVertexPosition);
-        gl.vertexAttribPointer(aVertexPosition, 2, gl.FLOAT, false, 0, 0);
-        gl.drawArrays(gl.TRIANGLES, 0, 6);
+        drawRect(x, y, width, height, true);
     }
 
     @Override
     public void drawRect(int x, int y, int width, int height) {
-        x -= canvas.getWidth() / 2;
-        y -= canvas.getHeight() / 2;
-
-        float canvasWidth = canvas.getWidth() / 2f;
-        float canvasHeight = canvas.getHeight() / 2f;
-
-        float startX = x / canvasWidth;
-        float startY = (y / canvasHeight);
-        float endX = (x + width) / canvasWidth;
-        float endY = ((y + height) / canvasHeight);
-
-        arr.set(new float[]{
-                startX, startY,
-                endX, startY,
-                endX, endY,
-                startX, endY
-        });
-        gl.useProgram(fill_rect);
-        WebGLUniformLocation uColor = gl.getUniformLocation(fill_rect, "uColor");
-        gl.uniform4fv(uColor, color);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, rect_buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, arr, gl.STATIC_DRAW);
-        gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indBuf);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-        gl.drawArrays(gl.LINE_LOOP, 0, 4);
+        drawRect(x, y, width, height, false);
     }
 
     @Override
@@ -194,5 +135,45 @@ public class JSGraphicsGL implements IGraphics {
         // draw the quad (2 triangles, 6 vertices)
         gl.drawArrays(gl.TRIANGLES, 0, 6);
 
+    }
+
+    private void drawRect(int x, int y, int width, int height, boolean fill) {
+        x -= canvas.getWidth() / 2;
+        y -= canvas.getHeight() / 2;
+
+        float canvasWidth = canvas.getWidth() / 2f;
+        float canvasHeight = canvas.getHeight() / 2f;
+
+        float startX = x / canvasWidth;
+        float startY = (y / canvasHeight);
+        float endX = (x + width) / canvasWidth;
+        float endY = ((y + height) / canvasHeight);
+
+        if (fill) {
+            arr.set(new float[]{
+                    startX, startY,
+                    startX, endY,
+                    endX, startY, // Triangle 1
+                    endX, startY,
+                    startX, endY,
+                    endX, endY // Triangle 2
+            });
+        } else {
+            arr.set(new float[]{
+                    startX, startY,
+                    endX, startY,
+                    endX, endY,
+                    startX, endY
+            });
+        }
+        gl.bindBuffer(gl.ARRAY_BUFFER, rect_buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, arr, gl.STATIC_DRAW);
+
+        gl.useProgram(draw_rect);
+        gl.uniform4fv(rectColorLocation, color);
+
+        gl.enableVertexAttribArray(aVertexPosition);
+        gl.vertexAttribPointer(aVertexPosition, 2, gl.FLOAT, false, 0, 0);
+        gl.drawArrays(fill ? gl.TRIANGLES : gl.LINE_LOOP, 0, fill ? 6 : 4);
     }
 }
